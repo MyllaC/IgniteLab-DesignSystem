0f8849e6d911b6a1a5aa6b1df8cad4b28a19e3e5
MyllaC <criscamila09@gmail.com>
2022-10-12 23:34:33 -0300

    componente e story de Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/global.css'
 
 export function App() {
-  return <h1 className='font-bold text-violet-500'>Hello World</h1>
+  return <h1 className='font-bold text-2xl text-cyan-500'>Hello World</h1>
 }
 
 
